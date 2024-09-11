e8f2abeeff60af7d31e0da68ea625bd493655aa9
Asik <mdasikuzzaman.en@gmail.com>
2024-09-11 13:11:04 +0600

    Update division-number.asm

--- division-number.asm
+++ division-number.asm
@@ -1,24 +1,23 @@
 .data
-    num1:   .word 20   # Dividend
-    num2:   .word 3    # Divisor
-    result: .word 0    # Quotient
-    rem:    .word 0    # Remainder
-
+    num1:   .word 200    # Dividend
+    num2:   .word 100    # Divisor
+   
 .text
-    main:
-        # Load dividend and divisor
-        lw   $t0, num1  # Load dividend into $t0
-        lw   $t1, num2  # Load divisor into $t1
-
-        # Perform division
-        div  $t0, $t1  # Divide $t0 by $t1
-
-        # Store results
-        mflo $t2       # Move quotient to $t2
-        mfhi $t3       # Move remainder to $t3
-        sw   $t2, result  # Store quotient
-        sw   $t3, rem     # Store remainder
-
-        # Exit program
-        li   $v0, 10   # Exit system call code
-        syscall        # Exit
+main:
+    # Load values
+    lw $t0, num1
+    lw $t1, num2
+    
+    # Perform division
+    div $t0, $t1        
+    
+    # Move the quotient
+    mflo $t2            
+    
+    li $v0, 1           
+    move $a0, $t2      
+    syscall             
+    
+    # Exit the program
+    li $v0, 10          
+    syscall
