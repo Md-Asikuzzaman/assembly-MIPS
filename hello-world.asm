0af944a7d7ecd47e00178d2cbd81903f5fd2d712
Asik <mdasikuzzaman.en@gmail.com>
2024-09-11 14:56:00 +0600

    Update hello-world.asm

--- hello-world.asm
+++ hello-world.asm
@@ -4,11 +4,11 @@
 
 .text
 main:
-    # Load the address of the string into register $a0
-    li   $v0, 4          # System call code for print_str
-    la   $a0, message    # Load address of the string
-    syscall              # Make the system call to print the string
+    
+    li   $v0, 4          
+    la   $a0, message    
+    syscall              
 
     # Exit the program
-    li   $v0, 10         # System call code for exit
-    syscall              # Make the system call to exit the program
+    li   $v0, 10        
+    syscall             
